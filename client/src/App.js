385046430b4e992f69f55dcfdc0ac2cab98474b8
rameshMonika <87105893+rameshMonika@users.noreply.github.com>
2025-06-29 20:22:59 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
     }
 
     try {
-      const response = await fetch(`/api/coins`, {
+      const response = await fetch(`http://localhost:5000/api/coins`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
